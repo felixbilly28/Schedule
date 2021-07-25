e58fa35531dfcd084579e1176f205e8e2b6e31eb
felixbilly28 <63266398+felixbilly28@users.noreply.github.com>
2021-07-24 22:29:39 -0400

    Add files via upload
    The schedule app

---
+++ Schedule/app/src/main/java/com/wv/myscheduler/MainActivity.java
@@ -0,0 +1,13 @@
+package com.wv.myscheduler;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
